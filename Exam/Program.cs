8bf3d8c5665b8897a5762e6376e5a33abf372c83
MohamedSalah10010 <abosalah100001000@gmail.com>
2024-10-19 20:44:56 +0300

    Add project files.

---
+++ Exam/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    class Question
+    {
+
+
+        public string questionBody { get; set; }
+        public int Mark { get; set; }
+        public int ID { get; set; }
+
+        public Question(int ID, string questionBody, int Mark)
+        {
+
+            this.ID = ID;
+            this.questionBody = questionBody;
+            this.Mark = Mark;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Question {ID}: {questionBody} ? ({Mark} Mark)";
+        }
+
+
+    }
+    class Choices
+    {
+
+
+        public string answerBody { get; set; }
+
+        public int Num { get; set; }
+
+        public Choices(int Num, string answerBody)
+        {
+
+            this.Num = Num;
+            this.answerBody = answerBody;
+
+        }
+
+        public override string ToString()
+        {
+            return $" Choice {Num}- {answerBody} )";
+        }
+
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<Question, List<Choices>> exam = new Dictionary<Question, List<Choices>>();
+            exam.Add(new Question(1, "What is the primary characteristic of a queue in data structures", 10),
+                new List<Choices>() { new Choices(1, "First In, Last Out (FILO)"), new Choices(2, "Last In, First Out (LIFO)"), new Choices(3, " First In, First Out (FIFO)"), new Choices(4, " Last In, Last Out (LILO)") });
+
+            exam.Add(new Question(2, "Which method in a queue is responsible for adding new elements", 20),
+               new List<Choices>() { new Choices(1, "pop()"), new Choices(2, "peek()"), new Choices(3, "push()"), new Choices(4, " remove()") });
+
+            exam.Add(new Question(3, "Which method in a queue allows you to view the front element without removing it", 15),
+                   new List<Choices>() { new Choices(1, "pop()"), new Choices(2, "peek()"), new Choices(3, "push()"), new Choices(4, " remove()") });
+
+
+
+            foreach (KeyValuePair<Question, List<Choices>> item in exam)
+            {
+
+
+                Console.WriteLine(item.Key);
+                foreach (Choices s in item.Value) {
+
+                    Console.WriteLine( s );
+
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FIFO/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIFO
+{
+
+    class Queue<thing> {
+
+        int lastIndex;
+        int firstIndex;
+        thing[] arr;
+
+        public Queue(int size = 10) {
+
+            arr = new thing[size];
+            firstIndex=0;
+            lastIndex = 0;
+        }
+
+        public void push(thing item) {
+
+            if (lastIndex < arr.Length)
+            {
+                arr[lastIndex++] = item;
+            }
+            else throw new InvalidOperationException("Queue is full.");
+
+        }
+        public thing pop() {
+
+            if (firstIndex < lastIndex)
+            {
+
+                return arr[firstIndex++];
+            }
+            else
+            {
+                firstIndex = 0;
+                lastIndex = 0;
+                return default(thing);
+            }
+        }
+
+
+    
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> s = new Queue<int>(5);
+            s.push(12);
+            s.push(4);
+            s.push(22);
+            Console.WriteLine(s.pop());
+            Console.WriteLine(s.pop());
+            Console.WriteLine(s.pop());
+            
+            s.push(1122);
+            s.push(450);
+           // s.push(20022);
+            Console.WriteLine(s.pop());
+            Console.WriteLine(s.pop());
+            Console.WriteLine(s.pop());
+            Queue<string> st = new Queue<string>();
+            st.push("mostafa");
+            st.push("ahmed");
+            st.push("Abosalah");
+            Console.WriteLine(st.pop());
+            Console.WriteLine(st.pop());
+            Console.WriteLine(st.pop());
+            Console.WriteLine(st.pop());
+            st.push("mostafa_again");
+            st.push("ahmed_last");
+            st.push("Abosalah_forever");
+            Console.WriteLine(st.pop());
+            Console.WriteLine(st.pop());
+            Console.WriteLine(st.pop());
+
+            Console.ReadKey();
+
+        }
+    }
+}
